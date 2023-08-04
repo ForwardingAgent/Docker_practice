61dd4bdd999bed46d40b97368bab89b743a715fa
ForwardingAgent <nikolaspdk@gmail.com>
2023-08-04 10:21:33 +0300

    migrations

--- service/service/settings.py
+++ service/service/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'clients'
-    
+    'clients',
+    'services'
 ]
 
 MIDDLEWARE = [
--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from clients.models import Client
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0,
+                                                   validators=[
+                                                       MaxValueValidator(100)
+                                                   ])
+
+
+class Subscription(models.Model):  # 3 видео 13:10
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT) 
+    # related_name - то имя с которым наша модел (класс Subscription) будет доступна внутри модели (Client) с которой мы тут образуем связь
+    # т.е. чтобы посмотреть все подписки клиента надо написать Client.subscriptions.all это при related_name
+    # второй способ посмотреть все подписки клиента: subscription.filter(client=client.id).all
+    service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
