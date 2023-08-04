c252e59fd95bfeedc5c6ea2af8d3653e4a2f4401
ForwardingAgent <nikolaspdk@gmail.com>
2023-08-04 09:07:19 +0300

    migrations

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    # 3 видео, к каждому юзеру в системе будет соответствовать модель (класс) Client
+    # on_delete - при удалении юзера проверит если связаные с ним модели Client и если есть то не даст удалить эту модель юзера
+    company_name = models.CharField(max_length=100)
+    full_address = models.CharField(max_length=100)
